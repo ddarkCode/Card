efd91ba6f473010a36560092b9f548f372332d59
Nnamani Ugochukwu F <royal.ugoh@gmail.com>
2020-05-21 11:52:49 +0100

    modified App and Form files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import CardList from './CardList'
 import Form from './Form';
-
+import axios from 'axios';
 
 class App extends Component {
     constructor(props) {
@@ -12,17 +12,26 @@ class App extends Component {
             profiles: []
         }
     }
+    handleSubmit = async (event) => {
+        event.preventDefault();
+        const resp = await axios.get(`https://api.github.com/users/${this.state.userName}`);
+        this.addNewProfile(resp.data);
+        this.setState({ userName: '' });
+    };
     addNewProfile = (profileData) => {
-        this.setState({
-            profiles: [profileData]
-        });
+        this.setState(prevState => ({
+          profiles: [...prevState.profiles, profileData],
+      }));
+    };
+    handleChange = (event) => {
+        this.setState({userName: event.target.value})
     }
 
     render() {
         return (
             <div>
                 <div className='header'>The GitHub Cards Ap"</div>
-                <Form onSubmit={this.addNewProfile} />
+                <Form userName={this.state.userName} handleInputChange={this.handleChange} handleInputSubmit={this.handleSubmit} />
                 <CardList profiles={this.state.profiles} />
             </div>
         )
--- src/Form.js
+++ src/Form.js
@@ -1,22 +1,16 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+
 
 
 class Form extends Component {
-	state = { userName: '' };
-	handleSubmit = async (event) => {
-  	event.preventDefault();
-    const resp = await axios.get(`https://api.github.com/users/${this.state.userName}`);
-    this.props.onSubmit(resp.data);
-    this.setState({ userName: '' });
-  };
+	
 	render() {
   	return (
-    	<form onSubmit={this.handleSubmit}>
+    	<form onSubmit={this.props.handleInputSubmit}>
     	  <input 
           type="text" 
-          value={this.state.userName}
-          onChange={event => this.setState({ userName: event.target.value })}
+          value={this.props.userName}
+          onChange={this.props.handleInputChange}
           placeholder="GitHub username" 
           required 
         />
